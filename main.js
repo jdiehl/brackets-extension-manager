1a388dbd92eade4d7e7bb54aaa59171f0ffdaeda
Jonathan Diehl <jdiehl@adobe.com>
2012-06-06 11:35:51 +0200

    Refactored the UI

--- main.js
+++ main.js
@@ -40,130 +40,20 @@ define(function (require, exports, module) {
         ExtensionLoader         = brackets.getModule("utils/ExtensionLoader");
 
     // Extension modules
-    var client = require("extensions-manager-client");
+    var ui = require("ui");
     
-    var $template   = null;
-    var $extensions;
-
-    Function.prototype.curry = function (callback) {
-        var args = Array.prototype.splice.call(arguments, 1, 0);
-        return callback.bind(this, args);
-    }
-
-    // click handler: switch between installed and active extensions
-    function _onTabClick() {
-        $this = $(this);
-        $this.siblings().removeClass("active");
-        $this.addClass("active");
-        $extensions.toggleClass("installed", $this.is(".installedTab"));
-    }
-
-    function _showManager() {
-        if (!$template) {
-            return;
-        }
-
-        // get the complete list of extensions (installed and available)
-        client.list(function (extensions) {
-
-            // create the dialog and find the elements to be customized inside it
-            var $dialog = $template.clone(true);
-            var $tabs = $dialog.find(".tabSwitcher");
-            $extensions = $dialog.find(".extensions").addClass("installed");
-            var $item = $extensions.find("> div:first").remove();
-            
-            $tabs.find(".tabSwitcher li").removeClass("active").click(_onTabClick);
-
-            $.each(extensions, function (index, extension) {
-                var $extension = $item.clone().appendTo($extensions);
-                
-                $extension
-                    .toggleClass("installed", typeof extension.status !== "undefined")
-                    .toggleClass("enabled", extension.status === 1)
-                    // Updating not yet supported
-                    .toggleClass("outdated", false);
-                
-                var $checkbox = $extension
-                    .find(".installationCheckbox")
-                    .attr("checked", $extension.is(".enabled"))
-                    .change(function () {
-                        var enable = this.checked;
-                        
-                        $extension.find(":input").attr("disabled", true);
-                        if ($extension.is(".installed")) {
-                            client[enable ? "enable" : "disable"](extension.name, function () {
-                                $extension.toggleClass("enabled", enable);
-                                $extension.find(":input").attr("disabled", false);
-                            });
-                        } else if (enable) {
-                            client.install(extension.name, function () {
-                                $extension.addClass("installed");
-                                $extension.addClass("enabled");
-                                $extension.find(":input").attr("disabled", false);
-                            });
-                        } else {
-                            $extension.find(":input").attr("disabled", false);
-                        }
-                    });
-                
-                $extension.find(".titleField").text(extension.title);
-                $extension.find(".descriptionField").text(extension.description);
-                $extension.find(".versionField").text(extension.version);
-                
-                $extension.find(".updateButton").click(function () {
-                    alert("Update is not yet implemented");
-                });
-                $extension.find(".uninstallButton").click(function () {
-                    client.uninstall(extension.name, function (res) {
-                        $extension.removeClass("installed");
-                        $extension.removeClass("enabled");
-                        $checkbox.attr('checked', false);
-                    });
-                });
-            });
-            
-            $dialog.find(".modal-footer").hide();
-            $dialog.find(".updateAllButton").attr("disabled", true).click(function () {
-                alert("Update all is not yet implemented");
-            });
-    
-            $dialog
-                .appendTo(window.document.body)
-                .modal({
-                    backdrop: "static",
-                    keyboard: true,
-                    show: true
-                });
-        });
-    }
-
     function _registerShortcut() {
         var commandId = "i10.extension_manager.test_modal";
-        CommandManager.register("Test Modal", commandId, _showManager);
+        CommandManager.register("Test Modal", commandId, ui.show);
         KeyBindingManager.addBinding(commandId, "Ctrl-Shift-E");
     }
     
-    function _loadStyle() {
-        $("<link rel='stylesheet' type='text/css'>").attr("href", extensionManagerDir + "main.css").appendTo(window.document.head);
-    }
-        
-    function _loadTemplate() {
-        $.get(extensionManagerDir + "main.html", function (template) {
-            // Append template to a DIV to make .find() work
-            $template = $("<div>").append(template).find(".modal");
-        });
-    }
-
     // Init the UI
     function init() {
-        _loadStyle();
-        _loadTemplate();
-
-        client.init(function () {
-            // Todo: set up extension manager commands here
+        ui.init(function () {
             _registerShortcut();
         });
     }
 
-    $(init);
+    init();
 });
---
+++ ui.js
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+	'use strict';
+
+	// Sadly hardcoded for now
+	var extensionDir = "extensions/user/";
+	var extensionManagerDir = extensionDir + "ExtensionManager/";
+	
+	// Brackets modules
+	var EditorManager           = brackets.getModule("editor/EditorManager"),
+		ProjectManager          = brackets.getModule("project/ProjectManager"),
+		Commands                = brackets.getModule("command/Commands"),
+		CommandManager          = brackets.getModule("command/CommandManager"),
+		KeyBindingManager       = brackets.getModule("command/KeyBindingManager"),
+		ExtensionLoader         = brackets.getModule("utils/ExtensionLoader");
+
+	// Extension modules
+	var client = require("extensions-manager-client");
+	
+	var _init = false;
+	var $dialog,
+		$extensions,
+		$item;
+
+
+	function _preventUserAction($element, callback) {
+		function release() {
+			$element.find(":input").attr("disabled", false);
+		}
+		$element.find(":input").attr("disabled", true);
+		callback(release);
+		if (callback.length === 0) release();
+	}
+
+	// click handler: switch between installed and active extensions
+	function _onTabClick(event) {
+		var $this = $(event.currentTarget);
+		$this.siblings().removeClass("active");
+		$this.addClass("active");
+		$extensions.toggleClass("installed", $this.is(".installedTab"));
+	}
+
+	// click handler: update all
+	function _onUpdateAllClick(event) {
+		alert("Update all is not yet implemented");
+	}
+
+	// click handler: update
+	function _onUpdateClick(event) {
+		alert("Update is not yet implemented");
+	}
+
+	// click handler: uninstall
+	function _onUninstallClick(event) {
+		var $this = $(event.currentTarget);
+		var $extension = $this.parent().parent();
+		var name = $extension.data("name");
+		_preventUserAction($extension, function (release) {
+			client.uninstall(name, function () {
+				$extension.removeClass("installed").removeClass("enabled");
+				$extension.find(".installationCheckbox").attr('checked', false);
+				release();
+			});
+		});
+	}
+	// change handler: install / enable / disable an extension
+	function _onExtensionChange(event) {
+		var $this = $(event.currentTarget);
+		var enable = $this.attr("checked");
+		var $extension = $this.parent();
+		var name = $extension.data("name");
+
+		// deativate all input elements for the extension while we are doing something
+		_preventUserAction($extension, function (release) {
+			// install the extension
+			if (!$extension.is(".installed")) {
+				client.install(name, function () {
+					$extension.addClass("installed").addClass("enabled");
+					release();
+				});
+			} else {
+				client[enable ? "enable" : "disable"](name, function () {
+					$extension.toggleClass("enabled", enable);
+					release();
+				});
+			}
+
+		});
+	}
+
+	// populate the dialog with extensions
+	function _populate(extensions) {
+		$.each(extensions, function (index, extension) {
+			var $extension = $item.clone().appendTo($extensions);
+			
+			// configure the css classes
+			$extension
+				.toggleClass("installed", typeof extension.status !== "undefined")
+				.toggleClass("enabled", extension.status === 1)
+				// Updating not yet supported
+				.toggleClass("outdated", false);
+			
+			// configure the check box
+			$extension.find(".installationCheckbox")
+				.attr("checked", $extension.is(".enabled"));
+			
+			// fill the content text
+			$extension.find(".titleField").text(extension.title);
+			$extension.find(".descriptionField").text(extension.description);
+			$extension.find(".versionField").text(extension.version);
+
+			// save the extension name
+			$extension.data("name", extension.name);
+		});
+	}
+
+	function _reset() {
+		$dialog.find(".tabSwitcher li").removeClass("active").first().click();
+		$extensions.children().remove();
+	}
+
+	function _loadStyle() {
+		$("<link rel='stylesheet' type='text/css'>").attr("href", extensionManagerDir + "ui.css").appendTo(window.document.head);
+	}
+		
+	function _loadTemplate(callback) {
+		$.get(extensionManagerDir + "ui.html", callback);
+	}
+
+	function _setupTemplate(template) {
+		// Append template to a DIV to make .find() work
+		$dialog = $("<div>").append(template).find(".modal");
+		$extensions = $dialog.find(".extensions").addClass("installed");
+		$item = $extensions.find("> div:first").remove();
+
+		// set up the click handlers
+		$dialog.find(".tabSwitcher").on("click", "li", _onTabClick);
+		$dialog.find(".updateAllButton").click(_onUpdateAllClick);
+		$extensions.on("click", ".updateButton", _onUpdateClick);
+		$extensions.on("click", ".uninstallButton", _onUninstallClick);
+		$extensions.on("change", ".installationCheckbox", _onExtensionChange);
+
+		// add to DOM
+		$dialog.hide().appendTo(window.document.body);
+		$dialog.modal({ backdrop: "static", keyboard: true, show: false });
+	}
+
+	// reset, show, and populate the dialog
+	function show() {
+		if (!_init) return;
+		_reset();
+		$dialog.modal();
+		client.list(_populate);
+	}
+
+	// Init the UI
+	function init(callback) {
+        $(function () {
+			client.init(function () {
+				_loadStyle();
+				_loadTemplate(function (template) {
+					_setupTemplate(template);
+					_init = true;
+					callback();
+				});
+			});
+        });
+	}
+
+	exports.init = init;
+	exports.show = show;
+});
