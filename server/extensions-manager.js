f1478b418c1e75fdec81fb41f84cb0c3bdbb6110
Jonathan Diehl <jonathan.diehl@gmail.com>
2012-06-13 15:56:15 +0200

    made extensions access async

--- server/extensions-manager.js
+++ server/extensions-manager.js
@@ -24,151 +24,155 @@
 var pathExtensions = __dirname + "/../../../";
 var pathDisabled = pathExtensions + "disabled/";
 var pathEnabled = pathExtensions + "user/";
-var databaseURL = __dirname + "/../database.json";
+var databaseURL = { host: "jdiehl.github.com", path: "/extensions.json" };
 
 var fs = require("./fs-extension");
+var http = require("http");
 var path = require("path");
 var exec = require("child_process").exec;
 var promise = require("node-promise/promise");
 
-// load and flag extensions
-var extensions;
-_loadExtensions();
-
-// create an index of the given names
-function _indexExtensions(index, names, status) {
-	for (var i in names) {
-		index[names[i]] = status;
-	}
-}
-
-// add status flag from the index to the extensions list
-function _flagExtensions(extensions, index) {
-	for (var i in extensions) {
-		var ext = extensions[i];
-		ext.status = index[ext.name];
-	}
-}
-
-function _loadExtensions() {
-	extensions = JSON.parse(fs.readFileSync(databaseURL));
+// index an array
+function _makeIndex(array, indexField) {
 	var index = {};
-	_indexExtensions(index, fs.readdirSync(pathDisabled), 0); // disabled
-	_indexExtensions(index, fs.readdirSync(pathEnabled), 1);     // enabled
-	_flagExtensions(extensions, index);
+	for (var i in array) {
+		var item = array[i];
+		index[item[indexField]] = item;
+	}
+	return index;
 }
 
-function _extensionWithName(name) {
-	for (var i in extensions) {
-		var ext = extensions[i];
-		if (ext.name === name) return ext;
+// set the status for extensions
+function _setStatus(extensions, names, status) {
+	for (var i in names) {
+		var name = names[i];
+		if (extensions[name]) extensions[name].status = status;
 	}
 }
 
-function _logError(error) {
-	console.error("\033[1m\033[31m[extensions-manager] " + error + "\033[0m");
+// load and flag extensions
+var _extensionsCache;
+function _loadExtensions(reload) {
+	var deferred = promise.defer();
+	if (reload) _extensionsCache = undefined;
+	else if (_extensionsCache) {
+		// serve the cache
+		deferred.resolve(_extensionsCache);
+	} else {
+		// load extensions.json
+		var req = http.request(databaseURL, function(res) {
+			data = "";
+			res.on("data", function (chunk) {
+				data += chunk.toString("utf8");
+			});
+			res.on("end", function () {
+				// parse, index, and flag the extensions data
+				data = JSON.parse(data);
+				_extensionsCache = _makeIndex(data, "name");
+				_setStatus(_extensionsCache, fs.readdirSync(pathDisabled), 0);
+				_setStatus(_extensionsCache, fs.readdirSync(pathEnabled), 1);
+				deferred.resolve(_extensionsCache);
+			});
+			res.on("error", deferred.reject.bind(null, deferred));
+		});
+		req.on("error", deferred.reject.bind(null, deferred));
+		req.end();
+	}
+	return deferred;
+}
+
+function _wrap(method, name) {
+	var deferred = promise.defer();
+	_loadExtensions().then(function (res) {
+		if (name) {
+			res = res[name];
+			if (!res) return promise.reject("Extension " + name + "not found.");
+		}
+		res = method(res, deferred);
+		if (method.length < 2) {
+			deferred.resolve(res);
+		}
+	}, deferred.reject.bind(null, deferred));
+	return deferred;
 }
 
 // get list of extensions and flag them as uninstalled, installed, or active
-function list() {
+function list(extensions) {
 	return extensions;
 }
 
-// install an extension
-function install(name) {
-	var ext = _extensionWithName(name);
-	if (!ext) return _logError("Extension " + name + "not found");
-
-	// clone the extension repository
-	var deferred = promise.defer();
-	var process = exec("/usr/bin/git clone " + ext.repository.url + " " + pathDisabled + name, function (res) {
+// install an extension (git clone)
+function install(ext, deferred) {
+	var process = exec("/usr/bin/git clone " + ext.repository.url + " " + pathDisabled + ext.name, function (res) {
 		ext.status = 0;
-		enable(name, ext);
+		enable(ext, deferred);
+	});
+}
+
+// uninstall (disable and delete) an extension
+function uninstall(ext, deferred) {
+	if (ext.status === 1) disable(ext);
+	fs.removeRecursive(pathDisabled + ext.name, function (err) {
+		if (err) return deferred.reject(err);
+		delete ext.status;
 		deferred.resolve();
 	});
-
-	return deferred.promise;
 }
 
-// uninstall an extension
-function uninstall(name) {
-	var ext = _extensionWithName(name);
-	if (!ext) return _logError("Extension " + name + "not found");
-
-	var deferred = promise.defer();
-	
-    if (ext.status === 1) disable(name, ext);
-    fs.removeRecursive(pathDisabled + name, function (err) {
-		if (err) return deferred.reject(err);
-		deferred.resolve();
-    });
-	delete ext.status;
-
-	return deferred.promise;
-}
-
-// enable an extension
-function enable(name, ext) {
-	if (path.existsSync(pathEnabled + name)) return _logError("Extension " + name + " is already enabled");
-
-	if (!ext) ext = _extensionWithName(name);
-	if (!ext) {
-		console.error("Extension " + name + " not found");
-		return;
+// enable an extension (create link)
+function enable(ext, deferred) {
+	if (!path.existsSync(pathEnabled + ext.name)) {
+		fs.symlinkSync("../disabled/" + ext.name, pathEnabled + ext.name);
 	}
-
-	// create link from disabled to enabled
-	fs.symlinkSync("../disabled/" + name, pathEnabled + name);
 	ext.status = 1;
+	deferred.resolve();
 }
 
 // disable an extension
-function disable(name, ext) {
-	if (!path.existsSync(pathEnabled + name)) return _logError("Extension " + name + " is not enabled");
-
-	if (!ext) ext = _extensionWithName(name);
-	if (!ext) return _logError("Extension " + name + "not found");
-
-	// delete link
-	var stats = fs.lstatSync(pathEnabled + name);
-	if (!stats.isSymbolicLink()) return _logError("Extension " + name + " is not installed as a link");
-	fs.unlinkSync(pathEnabled + name);
-	ext.status = 0;
+function disable(ext, deferred) {
+	if (path.existsSync(pathEnabled + ext.name)) {
+		var stats = fs.lstatSync(pathEnabled + ext.name);
+		if (!stats.isSymbolicLink()) {
+			return deferred.reject("Cannot disable extension " + ext.name + ": not installed as symbolic link");
+		}
+		fs.unlinkSync(pathEnabled + ext.name);
+		ext.status = 0;
+		deferred.resolve();
+	}
 }
 
-// update an extension
-function update(name, ext) {
-	if (!ext) ext = _extensionWithName(name);
-	if (!ext) return _logError("Extension " + name + " not found");
-	if (ext.status === undefined) return _logError("Extension " + name + " not installed");
-
-	// run git pull
-	var deferred = promise.defer();
+// update an extension (git pull)
+function update(ext, deferred) {
+	if (ext.status === undefined) {
+		return deferred.reject("Extension " + name + " not installed");
+	}
 	var process = exec("/usr/bin/git pull", {cwd: pathDisabled + name}, function (res) {
 		deferred.resolve();
 	});
-
-	return deferred.promise;
 }
 
 // update an extension
-function updateAll() {
+function updateAll(extensions, deferred) {
 	var promises = [];
 	for (var i in extensions) {
 		var ext = extensions[i];
-		if (ext.status === undefined) { continue; }
-		promises.push(update(ext.name, ext));
+		if (ext.status >= 0) {
+			var extDeferred = promise.defer();
+			promises.push(extDeferred);
+			update(ext, extDeferred);
+		}
 	}
-	return promise.all(promises);
+	promise.all(promises).then(deferred.resolve.bind(null, deferred),
+		deferred.reject.bind(null, deferred));
 }
 
 // public methods
 module.exports = {
-	list: list,
-	install: install,
-	uninstall: uninstall,
-	enable: enable,
-	disable: disable,
-	update: update,
-	updateAll: updateAll
+	list: _wrap(list),
+	install: _wrap(install),
+	uninstall: _wrap(uninstall),
+	enable: _wrap(enable),
+	disable: _wrap(disable),
+	update: _wrap(update),
+	updateAll: _wrap(updateAll)
 };
